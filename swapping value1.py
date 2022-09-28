3e08c7039c90756857bfa31b25c4154c016baba9
viveksingh75099 <114478652+viveksingh75099@users.noreply.github.com>
2022-09-28 22:41:05 +0530

    Create swapping value1.py

---
+++ swapping value1.py
@@ -0,0 +1,11 @@
+a=input("Enter Value of a:")
+b=input("Enter Value of b:")
+print("-"*50)
+print("Original value of a:{}".format(a))
+print("Original value of b:{}".format(b))
+print("-"*50)
+#swapping logic
+a,b=b,a  # Multi Line Assigment---swapping Operation
+print("Swapped value of a:{}".format(a))
+print("Swapped value of b:{}".format(b))
+print("-"*50)
