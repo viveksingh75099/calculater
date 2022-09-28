4b4159c048b114a566dde6625db1feb8068ae320
viveksingh75099 <114478652+viveksingh75099@users.noreply.github.com>
2022-09-28 13:28:00 +0530

    Create using if else programm.c

---
+++ using if else programm.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b;
+	printf("enter a number:");
+	scanf("%d",&a);
+	printf("enter b number:");
+	scanf("%d",&b);
+	if(a>b)
+	printf("a biggest number=%d",a);
+	else
+	printf("b biggest number=%d",b);
+	return 0;
+}
