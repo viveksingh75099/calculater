dda8a493d15cb5aeb496ef3b4d55acd760f0771a
viveksingh75099 <114478652+viveksingh75099@users.noreply.github.com>
2022-09-28 22:38:02 +0530

    Create value swapping.py

---
+++ value swapping.py
@@ -0,0 +1,13 @@
+a=input("Enter Value of a:")
+b=input("Enter Value of b:")
+print("-"*50)
+print("Original value of a:{}".format(a))
+print("Original value of b:{}".format(b))
+print("-"*50)
+#swapping logic
+t=a
+a=b
+b=t
+print("Swapped value of a:{}".format(a))
+print("Swapped value of b:{}".format(b))
+print("-"*50)
